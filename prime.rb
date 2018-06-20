13446b943da36f6c2978b054569f9b2bc090b286
Michael Blandford <mdblandford@gmail.com>
2018-06-20 20:32:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,16 @@
 require'pry'
 
 def prime?(integer)
-  if integer <= 1
-    return false
-  end
-  for n in 2..(integer-1)
-    integer % n == 0
-    return false
-  end
-else
-    true
-  end
+  all_numbers_greater_than_one = (2..integer-1)
+  all_numbers_greater_than_one.any? {|x| integer <=1 || integer % x == 0 ? false : true}
+  #return false
+  #end
+  #for n in 2..(integer-1)
+  #  integer % n == 0
+  #  return false
+  #end
+#else
+#    true
+end
   #for n loop --> creates the variable "n" within the string (2..(integer-1))
   # 2..(integer-1) creates an endless string of integers
