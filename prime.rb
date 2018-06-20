1e28b5b85f5bca86d2c007bfa827b6852f3e8983
Michael Blandford <mdblandford@gmail.com>
2018-06-20 20:09:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,15 @@ require'pry'
 
 def prime?(integer)
   if integer <= 1
-    return false 
-  end 
+    return false
+  end
   for n in 2..(integer-1)
-    if integer % n == 0 
-    return false 
+    if integer % n == 0
+    return false
     end
   end
 else
-  true 
+  true
 end
   #for n loop --> creates the variable "n" within the string (2..(integer-1))
   # 2..(integer-1) creates an endless string of integers
